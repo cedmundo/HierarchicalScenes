129b605c5a0d9681126cd1fd4cc7a7022d8bab12
Carlos Martínez <ced.mendoza@gmail.com>
2024-01-16 14:27:44 -0600

    render: Setup a simple render loop with an active camera

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <raylib.h>
 #include <flecs.h>
-#define WINDOW_HEIGHT 500
-#define WINDOW_WIDTH 500
+#define WINDOW_HEIGHT 800
+#define WINDOW_WIDTH 800
 #define WINDOW_TITLE "HierarchicalScenes"
 
 void PreUpdate(ecs_iter_t *it) {
@@ -17,9 +17,16 @@ void PostUpdate(ecs_iter_t *it) {
     }
 }
 
-void DrawGUI(ecs_iter_t *it) {
+void PreActiveCamera(ecs_iter_t *it) {
+    Camera3D *camera = ecs_field(it, Camera3D, 2);
+    // Use only first match of active camera
+    BeginMode3D(*camera);
+    DrawGrid(10, 1.0f);
+}
+
+void PostActiveCamera(ecs_iter_t *it) {
     (void) it;
-    DrawText("hello world", 100, 100, 12, WHITE);
+    EndMode3D();
 }
 
 int main(int argc, char **argv) {
@@ -32,16 +39,31 @@ int main(int argc, char **argv) {
     ECS_IMPORT(world, FlecsMonitor);
 
     // COMPONENTS
+    ECS_COMPONENT(world, Camera3D);
+
     // TAGS
+    ECS_TAG(world, ActiveCamera);
+
     // RELATIONSHIPS
     // SINGLETONS
     // SYSTEMS
     ECS_SYSTEM(world, PreUpdate, EcsPreFrame);
+    ECS_SYSTEM(world, PreActiveCamera, EcsPreFrame, ActiveCamera, Camera3D);
+    ECS_SYSTEM(world, PostActiveCamera, EcsPostFrame, ActiveCamera);
     ECS_SYSTEM(world, PostUpdate, EcsPostFrame);
-    ECS_SYSTEM(world, DrawGUI, EcsOnUpdate);
 
     // OBSERVERS
     // ENTITIES
+    ecs_entity_t camera_entity = ecs_entity(world, {});
+    ecs_set(world, camera_entity, Camera3D, {
+        .position = (Vector3) { 10, 10, 10 },
+        .target = (Vector3) { 0, 0, 0},
+        .up = (Vector3) { 0, 1.0f, 0 },
+        .fovy = 45.0f,
+        .projection = CAMERA_PERSPECTIVE,
+    });
+    ecs_add(world, camera_entity, ActiveCamera);
+
     ecs_app_run(world, &(ecs_app_desc_t) {
         .target_fps = 60,
         .enable_monitor = true,
