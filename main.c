d4dd80edc7a69b7c882ffd1bc43588552a4b6fe9
Carlos Martínez <ced.mendoza@gmail.com>
2024-01-16 14:04:05 -0600

    setup: Add raylib and flecs dependencies with hello world example

--- main.c
+++ main.c
@@ -1,6 +1,53 @@
-#include <stdio.h>
+#include <raylib.h>
+#include <flecs.h>
+#define WINDOW_HEIGHT 500
+#define WINDOW_WIDTH 500
+#define WINDOW_TITLE "HierarchicalScenes"
 
-int main() {
-    printf("Hello, World!\n");
+void PreUpdate(ecs_iter_t *it) {
+    (void) it;
+    BeginDrawing();
+    ClearBackground(BLACK);
+}
+
+void PostUpdate(ecs_iter_t *it) {
+    EndDrawing();
+    if (WindowShouldClose()) {
+        ecs_quit(it->world);
+    }
+}
+
+void DrawGUI(ecs_iter_t *it) {
+    (void) it;
+    DrawText("hello world", 100, 100, 12, WHITE);
+}
+
+int main(int argc, char **argv) {
+    ecs_world_t *world = ecs_init_w_args(argc, argv);
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+    SetWindowMonitor(0);
+
+    // IMPORTS
+    ECS_IMPORT(world, FlecsRest);
+    ECS_IMPORT(world, FlecsMonitor);
+
+    // COMPONENTS
+    // TAGS
+    // RELATIONSHIPS
+    // SINGLETONS
+    // SYSTEMS
+    ECS_SYSTEM(world, PreUpdate, EcsPreFrame);
+    ECS_SYSTEM(world, PostUpdate, EcsPostFrame);
+    ECS_SYSTEM(world, DrawGUI, EcsOnUpdate);
+
+    // OBSERVERS
+    // ENTITIES
+    ecs_app_run(world, &(ecs_app_desc_t) {
+        .target_fps = 60,
+        .enable_monitor = true,
+        .enable_rest = true,
+    });
+
+    CloseWindow();
     return 0;
 }
